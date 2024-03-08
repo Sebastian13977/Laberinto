5f11dba7dbd9ff5f7cebd37144d49ee33053dc3e
Sebastian <sebastianaramayo1102@gmail.com>
2024-03-07 22:56:44 -0300

    CONTROLES PERSONAJE PRINCIPAL

--- laberinto.py
+++ laberinto.py
@@ -16,11 +16,21 @@ PANTALLA_ALTO = 600
 screen = pygame.display.set_mode((PANTALLA_ANCHO,PANTALLA_ALTO))
 pygame.display.set_caption("Juego del Laberinto")
 
-def control_juego():
+def control_juego(player):
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+
+        if event.type == pygame.KEYDOWN or event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT:
+                player.mover(-2,0)
+            elif event.key == pygame.K_RIGHT:
+                player.mover(2,0)
+            elif event.key == pygame.K_UP:
+                player.mover(0,-2)
+            elif event.key == pygame.K_DOWN:
+                player.mover(0,2)
 #CREAR PERSONAJE
 player = PersonajePrincipal(50,50,30,30,AZUL)
 #JUEGO
@@ -28,7 +38,7 @@ player = PersonajePrincipal(50,50,30,30,AZUL)
 while True:
     screen.fill(VERDE)
 
-    control_juego()
+    control_juego(player)
     player.render(screen)
 
     pygame.display.flip()
